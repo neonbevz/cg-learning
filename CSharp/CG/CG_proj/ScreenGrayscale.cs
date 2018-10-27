8a82af49267e69c38b609f468a9b65c70b112a37
Adriyan Bevz <neonbevz@gmail.com>
2018-10-27 18:38:00 +0300

    Line drawing implementaiton

--- CSharp/CG/CG_proj/ScreenGrayscale.cs
+++ CSharp/CG/CG_proj/ScreenGrayscale.cs
@@ -48,84 +48,84 @@ public class ScreenGrayscale
         screen[x, y].SetValue(color);
     }
 
-//    public void DrawLine(int color, int x1, int y1, int x2, int y2)
-//    {
-//        int realX1 = x1;
-//        int realY1 = y1;
-//        int realX2 = x2;
-//        int realY2 = y2;
-//        
-//        if (x1 == x2)
-//        {
-//            if (y1 == y2)
-//            {
-//                realY1 = y2;
-//                realY2 = y1;
-//            }
-//
-//            for (int y = realY1; y < realY2; y++)
-//            {
-//                screen[x1, y].SetValue(color);
-//            }
-//            return;
-//        }
-//        else if (x1 > x2)
-//        {
-//            realX1 = x2;
-//            realY1 = y2;
-//            realX2 = x1;
-//            realY2 = y1;
-//        }
-//
-//        int deltaX = realX2 - realX1;
-//        int deltaY = realY2 - realY1;
-//
-//        bool horizontal = Math.Abs(deltaY) <= Math.Abs(deltaX);
-//
-//        if (!horizontal && realY1 > realY2)
-//        {
-//            
-//        }
+    public void DrawLine(int color, int x1, int y1, int x2, int y2)
+    {
+        int realX1 = x1;
+        int realY1 = y1;
+        int realX2 = x2;
+        int realY2 = y2;
         
-        
-//        if a[0] == b[0]:
-//        if a[1] > b[1]:
-//        a, b = b, a
-//        for y in range(a[1], b[1]):
-//        matrix[a[0]][y] = color
-//
-//        elif a[0] > b[0]:
-//        a, b = b, a
-//
-//            delta_x = b[0] - a[0]
-//        delta_y = b[1] - a[1]
-//
-//        horizontal = abs(delta_y) <= abs(delta_x)
-//
-//        if not horizontal and a[1] > b[1]:
-//        a, b = b, a
-//
-//            step_delta = delta_y / delta_x if horizontal else delta_x / delta_y
-//
-//        total_delta = 0
-//
-//        coord_b = a[1] if horizontal else a[0]
-//        range_start, range_end = (a[0], b[0]) if horizontal else (a[1], b[1])
-//
-//        for coord_a in range(range_start, range_end):
-//        if horizontal:
-//        matrix[coord_a][coord_b] = color
-//        else:
-//        matrix[coord_b][coord_a] = color
-//
-//        total_delta += step_delta
-//        if abs(total_delta) >= 0.5:
-//        coord_b += 1 if step_delta > 0 else -1
-//        total_delta += 1 if total_delta < 0 else -1
+        if (x1 == x2)
+        {
+            if (y1 == y2)
+            {
+                realY1 = y2;
+                realY2 = y1;
+            }
 
-//        
-//        return;
-//    }
+            for (int y = realY1; y < realY2; y++)
+            {
+                screen[x1, y].SetValue(color);
+            }
+            return;
+        }
+        else if (x1 > x2)
+        {
+            realX1 = x2;
+            realY1 = y2;
+            realX2 = x1;
+            realY2 = y1;
+        }
+
+        int deltaX = realX2 - realX1;
+        int deltaY = realY2 - realY1;
+
+        bool horizontal = Math.Abs(deltaY) <= Math.Abs(deltaX);
+
+        if (!horizontal && realY1 > realY2)
+        {
+            if (x1 > x2)
+            {
+                realX1 = x1;
+                realY1 = y1;
+                realX2 = x2;
+                realY2 = y2;
+            }
+            else
+            {
+                realX1 = x2;
+                realY1 = y2;
+                realX2 = x1;
+                realY2 = y1;
+            }
+        }
+
+        double stepDelta = (horizontal) ? (double) deltaY / deltaX : (double) deltaX / deltaY;
+
+        double totalDelta = 0d;
+
+        int coordB = horizontal ? realY1 : realX1;
+        int rangeStart = horizontal ? realX1 : realY1;
+        int rangeEnd = horizontal ? realX2 : realY2;
+
+        for (int coordA = rangeStart; coordA < rangeEnd; coordA++)
+        {
+            if (horizontal)
+                screen[coordA, coordB].SetValue(color);
+            else
+                screen[coordB, coordA].SetValue(color);
+
+            totalDelta += stepDelta;
+
+            if (Math.Abs(totalDelta) >= 0.5d)
+            {
+                coordB += (stepDelta > 0) ? 1 : -1;
+                totalDelta += (totalDelta < 0) ? 1 : -1;
+            }
+        }
+        
+        return;
+    }
     
     public int[,] AsIntArray()
     {
