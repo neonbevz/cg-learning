0f782cd2fa3740ad3d5801457b2ab6f169618709
Adriyan Bevz <neonbevz@gmail.com>
2018-10-27 13:37:09 +0300

    Add grayscale

---
+++ CSharp/CG/Grayscale.cs
@@ -0,0 +1,49 @@
+﻿public class Grayscale
+{
+    public int byteValue { get; private set; }
+
+    public Grayscale()
+    {
+        byteValue = 0;
+    }
+
+    public Grayscale(int newValue)
+    {
+        byteValue = CGMath.Clamp(newValue, 0, 255);
+    }
+
+    public Grayscale(float newValue)
+    {
+        byteValue = (int) (CGMath.Clamp(newValue, 0f, 1f) * 255);
+    }
+
+    public void SetValue(int newValue)
+    {
+        byteValue = CGMath.Clamp(newValue, 0, 255);
+    }
+
+    public void SetValue(float newValue)
+    {
+        byteValue = (int) (CGMath.Clamp(newValue, 0f, 1f) * 255);
+    }
+
+    public float AsFloat()
+    {
+        return (float) byteValue / 255;
+    }
+
+    public static Grayscale operator + (Grayscale grayscale1, Grayscale grayscale2)
+    {
+        return new Grayscale(grayscale1.byteValue + grayscale2.byteValue);
+    }
+    
+    public static Grayscale operator - (Grayscale grayscale1, Grayscale grayscale2)
+    {
+        return new Grayscale(grayscale1.byteValue - grayscale2.byteValue);
+    }
+    
+    public static Grayscale operator * (Grayscale grayscale1, Grayscale grayscale2)
+    {
+        return new Grayscale(grayscale1.AsFloat() * grayscale2.AsFloat());
+    }
+}
